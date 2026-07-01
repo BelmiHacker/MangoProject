6f27d98d5eb924dda9c57a7ac0d8a509c4ad0cad
Belmiro Kayru <belmiro.wibi@gmail.com>
2026-07-01 22:41:53 +0700

    Initial Commit

---
+++ MangoProject/MangoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangoProjectApp.swift
+//  MangoProject
+//
+//  Created by Belmiro Kayru on 01/07/26.
+//
+
+import SwiftUI
+
+@main
+struct MangoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
